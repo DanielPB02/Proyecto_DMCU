9194eea8141d4d8eb406e9f310bd0097088d4f52
MauricioRC_ <mauriciorodricar@hotmail.com>
2019-11-05 23:47:56 -0600

    Ya está esta parte, seguiré llorando

--- app/src/main/java/com/example/proyecto_dmcu/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_dmcu/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.proyecto_dmcu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.login_screen.*
+import kotlinx.android.synthetic.main.signin_screen.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
+
+        b_signin.setOnClickListener {
+            showSignin()
+        }
+
+        b_accept.setOnClickListener{
+            login()
+        }
     }
+
+    private fun showSignin() {
+        setContentView(R.layout.signin_screen)
+        b_haveAcc.setOnClickListener {
+            showLogin()
+        }
+    }
+
+    private fun showLogin() {
+        setContentView(R.layout.login_screen)
+        b_signin.setOnClickListener {
+            showSignin()
+        }
+
+    }
+
+    private fun login() {
+        setContentView(R.layout.welcome)
+        b_signin.setOnClickListener {
+            showSignin()
+        }
+
+    }
+
 }
